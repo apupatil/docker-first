fcbb7b09ec571bee10ad0c61d04c709574bad09e
apupatil <96patil.apurva@gmail.com>
2021-04-04 18:48:00 +0530

    chane app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>You will get 15 lac package next month by redhat!</p>
+        <p>You will get 15 lac package next month!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
